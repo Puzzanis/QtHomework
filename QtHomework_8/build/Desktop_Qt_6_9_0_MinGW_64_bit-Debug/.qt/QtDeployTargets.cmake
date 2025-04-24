5ae95e83abb0bdebc0cf0e7cb6d1f4a800e761bd
Puzzanis <capitalsvk@gmail.com>
2025-04-24 07:48:00 +0300

    refactor

--- QtHomework_8/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_08_PreDataBase_FILE E:/C++/QtHomework/QtHomework_8/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/08_PreDataBase.exe)
-set(__QT_DEPLOY_TARGET_08_PreDataBase_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_08_PreDataBase_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
